c090e7b3e1f585466711d841f8e045baa980aa82
Ankit Solanki <ankit03june@gmail.com>
2018-05-01 15:59:16 +0530

    ADD: new function to scrape synonyms of a word

--- script.py
+++ script.py
@@ -31,11 +31,38 @@ def rhythmic_words(word, num):
 				print("( "+wordlist[i][1]+", "+wordlist[i][2]+" )")
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--rhyme", help="get rhyming words", type=str, required=True)
+def synonyms(word, num):
+	print("\n [*] Getting synonyms for the word: ", word, "...")
+
+	url = "http://www.thesaurus.com/browse/"+word
+
+	raw = requests.get(url)
+	soup = BeautifulSoup(raw.text, "lxml")
+
+	section = soup.find_all('section')
+	ul = section[0].find_all('ul')
+	li = ul[0].find_all('li')
+
+	for element in li:
+		for x in element:
+			x = x.text.strip()
+			if not x.startswith('.css'):
+				print(x)
+			else:
+				temp_list = x.split('}')
+				print(temp_list[len(temp_list)-1])
+
+
+parser = argparse.ArgumentParser(description='PyPoet: Play with words')
+parser.add_argument("word", help="an input of the word")
+parser.add_argument("-r", "--rhyme", help="get rhyming words", action="store_true")
+parser.add_argument("-s", "--synonym", help="get synonym words", action="store_true")
 parser.add_argument("-n", "--number", type=int, help="number of words should be returned", default=20)
 
 args = parser.parse_args()
 
-if len(args.rhyme)!=0:
-	rhythmic_words(args.rhyme, args.number)
+if (args.rhyme):
+	rhythmic_words(args.word, args.number)
+
+if (args.synonym):
+	synonyms(args.word, args.number)
